651d0938b7b555f9a6a4b9e3be7ac923166b3673
LowSociety <fredrik@properdesign.se>
2015-12-07 17:03:07 +0100

    Fixed incorrect checkin.

--- src/ajax.js
+++ src/ajax.js
@@ -303,7 +303,7 @@ jQuery.extend( {
 		processData: true,
 		async: true,
 		contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-		m : "_",
+		cacheParam : "_",
 		/*
 		timeout: 0,
 		data: null,
